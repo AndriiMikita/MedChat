70421da8f1fddd18a8cd25c823c019c7d3d5ba90
AndriiMikita <mikita.andrii.r@gmail.com>
2023-10-18 13:09:02 +0000

    Updated lots of things

--- src/main.py
+++ src/main.py
@@ -1,24 +1,14 @@
 import os
 import sys
 import openai
+from langchain.llms import Ollama
+from langchain import FAISS
 from environs import Env
-from langchain.llms import HuggingFacePipeline
 from langchain.document_loaders import UnstructuredXMLLoader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OllamaEmbeddings
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from settings import DATA_DIR_PATH, ENV_DIR_PATH
-
-env = Env()
-env.read_env(os.path.join(ENV_DIR_PATH, '.env'))
-openai.api_key = env.str("HUGGINGFACE_API_TOKEN")
-
-def setup_huggingface_pipeline(model_id, task, pipeline_kwargs):
-    hf_pipeline = HuggingFacePipeline.from_model_id(
-        model_id=model_id,
-        task=task,
-        pipeline_kwargs=pipeline_kwargs,
-    )
-    return hf_pipeline
 
 def run_llm(query: str):
     data_path = os.path.join(DATA_DIR_PATH, 'test_data.xml')
@@ -28,12 +18,21 @@ def run_llm(query: str):
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     chunks = text_splitter.split_documents(documents=docs)
 
-    model_id = "gpt2"
-    task = "text-generation"
-    pipeline_kwargs = {"max_new_tokens": 10}
-    hf_pipeline = setup_huggingface_pipeline(model_id, task, pipeline_kwargs)
+    embeddings = OllamaEmbeddings(model="llama2:7b",)
+    index_name = "med-chat"
 
-    result = hf_pipeline(query)
+    try:
+        vectorstore = FAISS.load_local(index_name, embeddings)
+    except Exception as e:
+        print("Creating index...")
+        vectorstore = FAISS.from_documents(chunks, embeddings)
+        vectorstore.save_local(index_name)
+        print("The index was created successfully")
+
+
+    model = "llama2"
+    ollama = Ollama(model=model)
+    result = ollama(prompt=query)
     return result
 
 
